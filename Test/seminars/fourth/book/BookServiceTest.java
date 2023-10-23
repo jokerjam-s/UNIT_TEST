e0f774a271c17f27691e1732b98de0e8523488f9
Joker Jam <jokerjam@yandex.ru>
2023-10-24 01:30:00 +0300

    work 04 ready

--- Test/seminars/fourth/book/BookServiceTest.java
+++ Test/seminars/fourth/book/BookServiceTest.java
@@ -1,5 +1,68 @@
 package seminars.fourth.book;
 
-public class BookServiceTest {
+import org.assertj.core.api.ObjectArrayAssert;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+public class BookServiceTest {
+    private Book book1;
+    private Book book2;
+
+    @BeforeEach
+    void setUp(){
+        book1 = new Book("1", "Book1", "Author1");
+        book2 = new Book("2", "Book2", "Author2");
+    }
+
+    @Test
+    void testBookServiceNotBook2(){
+        InMemoryBookRepository bookRepository = mock(InMemoryBookRepository.class);
+        BookService bookService = new BookService(bookRepository);
+
+        when(bookRepository.findById("1")).thenReturn(book1);
+        when(bookRepository.findById("2")).thenReturn(book2);
+
+        String bookResult = bookService.findBookById("1").getId();
+
+        verify(bookRepository, times(1)).findById("1");
+        assertThat(bookResult).isNotEqualTo("2");
+    }
+
+    @Test
+    void testBookServiceBook2(){
+        InMemoryBookRepository bookRepository = mock(InMemoryBookRepository.class);
+        BookService bookService = new BookService(bookRepository);
+
+        when(bookRepository.findById("1")).thenReturn(book1);
+        when(bookRepository.findById("2")).thenReturn(book2);
+
+        String bookResult = bookService.findBookById("2").getId();
+
+        verify(bookRepository, times(1)).findById("2");
+        assertThat(bookResult).isEqualTo("2");
+    }
+
+    @Test
+    void testBookServiceList(){
+        List<Book> books = new ArrayList<>(){{ add(book1); add(book2);}};
+
+        InMemoryBookRepository bookRepository = mock(InMemoryBookRepository.class);
+        BookService bookService = new BookService(bookRepository);
+
+        when(bookRepository.findAll()).thenReturn(books);
+
+        List<Book> bookResult = bookService.findAllBooks();
+
+        verify(bookRepository, times(1)).findAll();
+        assertIterableEquals(bookResult, books);
+    }
 }
--- src/seminars/fourth/book/InMemoryBookRepository.java
+++ src/seminars/fourth/book/InMemoryBookRepository.java
@@ -12,8 +12,6 @@ public class InMemoryBookRepository implements BookRepository  {
         this.books = new HashMap<>();
         books.put("1", new Book("1", "Book1", "Author1"));
         books.put("2", new Book("2", "Book2", "Author2"));
-        books.put("3", new Book("3", "Book3", "Author1"));
-        books.put("4", new Book("4", "Book4", "Author3"));
     }
 
     @Override
